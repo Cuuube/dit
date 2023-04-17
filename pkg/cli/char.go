4dd51a4680e5be70f24f8d27ecff5d7295c517ac
zhangxiaodong.zxod <zhangxiaodong.zxod@bytedance.com>
2023-04-17 20:40:20 +0800

    update

---
+++ pkg/cli/char.go
@@ -0,0 +1,47 @@
+package cli
+
+const (
+	ResetStyle = "\033[0m"
+
+	StyleNormal      = "\033[0m"
+	StyleBold        = "\033[1m"
+	StyleDark        = "\033[2m"
+	StyleItalic      = "\033[3m"
+	StyleUnderline   = "\033[4m"
+	StyleShine       = "\033[5m"
+	StyleRevertColor = "\033[7m"
+	StyleInvisible   = "\033[8m"
+
+	ColorBlack   = "\033[30m"
+	ColorRed     = "\033[31m"
+	ColorGreen   = "\033[32m"
+	ColorYellow  = "\033[33m"
+	ColorBlue    = "\033[34m"
+	ColorMagenta = "\033[35m"
+	ColorCyan    = "\033[36m"
+	ColorWhite   = "\033[37m"
+
+	BgColorBlack   = "\033[40m"
+	BgColorRed     = "\033[41m"
+	BgColorGreen   = "\033[42m"
+	BgColorYellow  = "\033[43m"
+	BgColorBlue    = "\033[44m"
+	BgColorMagenta = "\033[45m"
+	BgColorCyan    = "\033[46m"
+	BgColorWhite   = "\033[47m"
+
+	CursorUpLine1     = "\033[1A"
+	CursorUpLine2     = "\033[2A"
+	CursorUpFormat    = "\033[%dA"
+	CursorDownFormat  = "\033[%dB"
+	CursorRightFormat = "\033[%dC"
+	CursorLeftFormat  = "\033[%dD"
+
+	CursorSavePosition = "\033[s"
+	CursorLoadPosition = "\033[u"
+
+	CursorSetPositionFormat = "\033[%d;%dH" // y, x
+
+	ClearLine   = "\033[K"
+	ClearScreen = "\033[2J"
+)
--- pkg/cli/scan.go
+++ pkg/cli/scan.go
@@ -1,15 +1,42 @@
 package cli
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// func Scan() string {
+// 	var inp string
+// 	_, err := fmt.Scanln(&inp)
+// 	if err != nil {
+// 		// Println("读取输入出错:", err)
+// 		return ""
+// 	}
+// 	return inp
+// }
 
 func Scan() string {
-	var inp string
-	_, err := fmt.Scanln(&inp)
-	if err != nil {
-		// Println("读取输入出错:", err)
-		return ""
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		// reads user input until \n by default
+		scanner.Scan()
+		// Holds the string that was scanned
+		text := scanner.Text()
+		if len(text) != 0 {
+			return text
+		} else {
+			// exit if user entered an empty string
+			break
+		}
+
 	}
-	return inp
+
+	// handle error
+	if scanner.Err() != nil {
+		fmt.Println("read input error: ", scanner.Err())
+	}
+	return ""
 }
 
 func ReadInput() string {
